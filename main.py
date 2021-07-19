17cd5b8845d964f10b604965bc419f22bb9da23e
MaksedCat <carjien@gmail.com>
2021-07-19 11:48:36 +0200

    Screen update fixed

--- main.py
+++ main.py
@@ -126,6 +126,7 @@ fmap = FMAP(20,10)
 while True:
     put = getinput()
     players[0] = players[0].update(put)
+    os.system('cls')
     print((players[0].hasball, players[0].number, players[0].pos, players[0].stamina))
     tmap = fmap.update(players, fmap.y, fmap.x)
     for i in range(1, len(players)):
